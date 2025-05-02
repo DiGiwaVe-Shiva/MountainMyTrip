80bfdb5c418eb7e10ef472362b9aa356c3ea054c
DiGiwaVe-Shiva <digiwaveseo@gmail.com>
2025-05-02 13:43:48 +0530

    himalayantripchangeF

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
     <>
       <main>
          {/* Ensure this stays on top */}
-         <section className="relative z-20">
+         <section className="relative z-50">
           <From />
         </section>
 
