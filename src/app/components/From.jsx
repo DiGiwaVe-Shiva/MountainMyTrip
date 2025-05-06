1cf78a1cae49201421c54fa7ba8d7489d7ea3b53
DiGiwaVe-Shiva <digiwaveseo@gmail.com>
2025-05-06 15:08:32 +0530

    doneb

--- src/app/components/From.jsx
+++ src/app/components/From.jsx
@@ -1290,21 +1290,14 @@ export default function Form() {
           </div>
 
           {/* Travel Date */}
-          <div className="flex flex-col w-full">
-  <label htmlFor="travelDate" className="text-xs text-gray-500 mb-1">
-    Travel Date
-  </label>
-  <div className="relative w-full">
-    <FaCalendarAlt className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
-    <input
-      id="travelDate"
-      type="date"
-      className="w-full border border-gray-300 rounded-md pl-10 pr-3 py-2 bg-white text-sm min-h-[44px] outline-none"
-      value={travelDate}
-      onChange={(e) => setTravelDate(e.target.value)}
-    />
-  </div>
-</div>
+          <input
+  id="travelDate"
+  type="date"
+  className="w-full min-w-full border border-gray-300 rounded-md pl-10 pr-3 py-2 bg-white text-sm min-h-[44px] outline-none"
+  value={travelDate}
+  onChange={(e) => setTravelDate(e.target.value)}
+/>
+
 
 
           {/* Passengers */}
