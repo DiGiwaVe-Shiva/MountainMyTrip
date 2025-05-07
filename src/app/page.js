0de412b3cdaa3ab12a1eedbbb5b64a5d19f882ad
DiGiwaVe-Shiva <digiwaveseo@gmail.com>
2025-05-07 18:00:46 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -6,23 +6,21 @@ import Planyour from "./components/Planyour";
 import HimTrek from "./components/HimTrek";
 import Testimonials from "./components/Testimonials";
 import Faq from "./components/Faq";
-// import PopupForm from "./components/PopupForm";
-// import Navbar2 from "./components/Navbar2";
+
 
 export default function Home() {
-  // const loading = true;
+  
 
   return (
     <>
       <main>
-        {/* Ensure this stays on top */}
+       
         <section className="relative z-20">
-          {/* <Navbar2 /> */}
-          {/* <PopupForm /> */}
+         
           <From />
         </section>
 
-        {/* Carousel with lower z-index to prevent overlapping on iOS */}
+        
         <section className="relative z-10">
           <HimaliyeMyTrip />
         </section>
